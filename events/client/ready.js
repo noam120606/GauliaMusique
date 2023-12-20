2873f75e1dd88ea02dffc10f9a3178b3f12b7b8a
noam120606 <noamroger120606@gmail.com>
2023-12-20 23:27:20 +0100

    reupload + remove 75 serv obj

--- events/client/ready.js
+++ events/client/ready.js
@@ -5,13 +5,15 @@ module.exports = {
     name: Events.ClientReady,
     async run(client) {
 
+        client.disstat = new DisStat(process.env.DISSTATtoken, client);
+
         client.application.commands.set(client.commands.map(command => command.data));
-        if (!client.dev) client.disstat = new DisStat(process.env.DISSTATtoken, client);
 
         client.user.setActivity(`${client.guilds.cache.size} serveurs`, {type: ActivityType.Watching});
         setInterval(() => {
             client.user.setActivity(`${client.guilds.cache.size} serveurs`, {type: ActivityType.Watching});
         }, 1 * 60 * 60 * 1000)
+
         console.log(`[startup] ${client.user.username} est en ligne`);
     }
 };
