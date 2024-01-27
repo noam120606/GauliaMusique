fd6818948d7d7ddbedec66b599f206658818c641
noam120606 <noamroger120606@gmail.com>
2024-01-27 22:04:15 +0100

    mise en place stats dashboard

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ const loadInteractions = require('./loaders/loadInteractions');
 const { Player } = require('discord-player');
 const { Client, IntentsBitField, Collection } = require('discord.js');
 const client = new Client({ intents: new IntentsBitField(process.env.INTENTS) });
+const Poster = new Stats.Client(client, {
+    stats_uri: 'http://gaulia-stats.noam120606.fr:20003/',
+    authorizationkey: process.env.STATSAUTH,
+})
 client.dev = process.env.DEVBOT=="1";
 client.commands = new Collection();
 client.interactions = new Collection();
